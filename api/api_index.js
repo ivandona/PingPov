0e0e844bad846c77ae863a11d2400194385b8bfe
Davide Nardi <davide.nardi-1@studenti.unitn.it>
2022-05-13 15:26:06 +0200

    First try

---
+++ api/api_index.js
@@ -0,0 +1,9 @@
+const apiBooks = require('./books');
+const apiAuth = require('./google_login');
+module.exports = function(app,mongoose) {
+  apiBooks(app,mongoose);
+  apiAuth(app);
+};
+
+
+
--- api/index.js
+++
@@ -1,6 +0,0 @@
-// api/index.js
-const apiBooks = require('./books');
-module.exports = function(app,mongoose) {
-  apiBooks(app,mongoose);
- // other routes
-};
